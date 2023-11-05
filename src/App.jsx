14c1773ba54b93fe776f00e04668bdeb2f3beec8
GhostMobin <s1810677110@ru.ac.bd>
2023-11-05 16:42:08 +0600

    checkbox added

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ const App = () => {
     <div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-6">
+        <img className="col-span-2 row-span-2" src="https://i.ibb.co/S6RZfK9/image-10.jpg" alt="" />
         {
           images.map(image => (
-            <div key={image.id}>
+            <div key={image.id} className="border rounded-xl overflow-hidden relative">
               <img src={image.url} alt="" />
+              <input type="checkbox" name={image.id} className="bg-white checkbox absolute top-6 left-6" />
+
             </div>
           ))
         }
